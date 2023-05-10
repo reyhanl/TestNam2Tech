d703fd89446579c19fff3fbb5f1c003feb3a3fab
reyhan muhammad <reyhanmuhammad@192.168.1.4>
2023-05-10 08:47:02 +0700

    clean and refactor code

--- Test/DetailViewController.swift
+++ Test/DetailViewController.swift
@@ -11,20 +11,20 @@ import MapKit
 class DetailViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var imagesCollectionView: UICollectionView!
+    @IBOutlet weak var imagesCarouselCollectionView: UICollectionView!
     @IBOutlet weak var ratingCollectionView: UICollectionView!
     @IBOutlet weak var mapKit: MKMapView!
     @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var appleMapActionView: UIView!
     @IBOutlet weak var googleMapActionLabel: UILabel!
     
-    var id: String?
-    var businessModel: BusinessModel?{
+    private var id: String?
+    private var businessModel: BusinessModel?{
         didSet{
             updateData()
         }
     }
-    var ratings: [Rating]?{
+    private var ratings: [Rating]?{
         didSet{
             updateRatingCollectionView()
         }
@@ -34,8 +34,9 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         setupMapKit()
         setCollectionView()
-        fetchData(id: id ?? "")
-        fetchRating(id: id ?? "")
+        let id = id ?? ""
+        fetchData(id: id)
+        fetchRating(id: id)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,27 +47,27 @@ class DetailViewController: UIViewController {
         self.id = id
     }
     
-    func setupMapKit(){
+    private func setupMapKit(){
         googleMapActionLabel.addGestureRecognizer(target: self, selector: #selector(openOnGoogleMaps))
         appleMapActionView.addGestureRecognizer(target: self, selector: #selector(openOnGoogleMaps))
     }
     
-    func setCollectionView(){
-        imagesCollectionView.dataSource = self
-        imagesCollectionView.delegate = self
-        imagesCollectionView.decelerationRate = .fast
+    private func setCollectionView(){
+        imagesCarouselCollectionView.dataSource = self
+        imagesCarouselCollectionView.delegate = self
+        imagesCarouselCollectionView.decelerationRate = .fast
         
         ratingCollectionView.dataSource = self
         ratingCollectionView.delegate = self
         
-        let nib = UINib(nibName: "ImageCollectionViewCell", bundle: nil)
-        imagesCollectionView.register(nib, forCellWithReuseIdentifier: "cell")
+        let ImageCarouselCell = UINib(nibName: "ImageCollectionViewCell", bundle: nil)
+        imagesCarouselCollectionView.register(ImageCarouselCell, forCellWithReuseIdentifier: "cell")
         
         let ratingCell = UINib(nibName: "RatingCollectionViewCell", bundle: nil)
         ratingCollectionView.register(ratingCell, forCellWithReuseIdentifier: "cell")
     }
     
-    func fetchData(id: String){
+    private func fetchData(id: String){
         NetworkManager.shared.fetchBusinessDetail(id: id) { result in
             switch result {
             case .success(let model):
@@ -77,11 +78,10 @@ class DetailViewController: UIViewController {
         }
     }
     
-    func fetchRating(id: String){
+    private func fetchRating(id: String){
         NetworkManager.shared.fetchBusinessRating(id: id, completion: { result in
             switch result{
             case .success(let response):
-                print(response.reviews)
                 self.ratings = response.reviews
             case .failure(let error):
                 print(error.localizedDescription)
@@ -89,17 +89,17 @@ class DetailViewController: UIViewController {
         })
     }
     
-    func updateData(){
+    private func updateData(){
         guard let businessModel = businessModel else{return}
         DispatchQueue.main.async {
-            self.imagesCollectionView.reloadData()
+            self.imagesCarouselCollectionView.reloadData()
             self.titleLabel.text = businessModel.name ?? ""
             self.ratingLabel.text = "★ \(businessModel.rating ?? 0)"
             self.setMapKit(long: businessModel.coordinates?.longitude, lat: businessModel.coordinates?.latitude)
         }
     }
     
-    func setMapKit(long: Float?, lat: Float?){
+    private func setMapKit(long: Float?, lat: Float?){
         guard let long = long, let lat = lat else{return}
         mapKit.centerCoordinate = .init(latitude: CLLocationDegrees(lat), longitude: CLLocationDegrees(long))
         mapKit.cameraZoomRange = MKMapView.CameraZoomRange.init(maxCenterCoordinateDistance: 30)
@@ -110,8 +110,11 @@ class DetailViewController: UIViewController {
         googleMapActionLabel.isHidden = false
     }
     
-    func scrollToNearestCell(){
-        guard let collectionView = imagesCollectionView, let index = collectionView.indexPathsForVisibleItems.sorted().last, let firstIndex = collectionView.indexPathsForVisibleItems.sorted().first else{return}
+    private func scrollToNearestCell(){
+        //Since the image/cell take up the entire width of the screen, we can assume that at any given point, there will only be two image being displayed at the same time.
+        //Then, we can just get all the IndexPath for the visible cell, and see its Global position to determine which image to scroll to (based on which one is bigger)
+        guard let collectionView = imagesCarouselCollectionView, let index = collectionView.indexPathsForVisibleItems.sorted().last, let firstIndex = collectionView.indexPathsForVisibleItems.sorted().first else{return}
+        //We use global position to make sure that the origin.x is going to be based on the entire screen, instead of its superview
         if let cell = collectionView.cellForItem(at: index), let globalFrame = cell.globalFrame{
             if globalFrame.origin.x < view.frame.width / 2{
                 collectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
@@ -121,14 +124,18 @@ class DetailViewController: UIViewController {
         }
     }
     
-    func updateRatingCollectionView(){
+    private func updateRatingCollectionView(){
         DispatchQueue.main.async {
             self.ratingCollectionView.reloadData()
         }
     }
     
-    @objc func openOnGoogleMaps(){
-        guard let long = businessModel?.coordinates?.longitude, let lat = businessModel?.coordinates?.latitude, let url = URL(string: "comgooglemaps://?saddr=&daddr=\(lat),\(long)&directionsmode=driving"), let googleUrl = URL(string: "comgooglemaps://") else{return}
+    @objc private func openOnGoogleMaps(){
+        guard
+            let long = businessModel?.coordinates?.longitude,
+            let lat = businessModel?.coordinates?.latitude,
+            let url = URL(string: "comgooglemaps://?saddr=&daddr=\(lat),\(long)&directionsmode=driving"),
+            let googleUrl = URL(string: "comgooglemaps://") else{return}
         if UIApplication.shared.canOpenURL(googleUrl){
             UIApplication.shared.open(url)
         }
@@ -137,7 +144,7 @@ class DetailViewController: UIViewController {
 
 extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if collectionView === self.imagesCollectionView{
+        if collectionView === self.imagesCarouselCollectionView{
             if let businessModel = businessModel, let photos = businessModel.photos{
                 let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ImageCollectionViewCell
                 cell.setupCell(urlString: photos[indexPath.row])
@@ -154,7 +161,7 @@ extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSo
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView === self.imagesCollectionView{
+        if collectionView === self.imagesCarouselCollectionView{
             guard let businessModel = businessModel, let photos = businessModel.photos else{return 0}
             return photos.count
         }else{
@@ -168,7 +175,7 @@ extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSo
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        if collectionView === self.imagesCollectionView{
+        if collectionView === self.imagesCarouselCollectionView{
             return CGSize(width: view.frame.width, height: 200)
         }else{
             return CGSize(width: 200, height: 150)
